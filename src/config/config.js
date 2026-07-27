309217a03a48d7264d4ae043c42c4dae30b5efb2
boncabr <ariboncabe100@gmail.com>
2026-07-27 22:08:58 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -51,7 +51,7 @@ module.exports = {
     searchPlatform: 'ytsearch',
     leaveOnEmptyDelay: 30000,
     leaveOnEndDelay: 30000,
-    voiceChannelBitrate: 256000,
+    voiceChannelBitrate: 320000,
   },
 
   cooldowns: {
